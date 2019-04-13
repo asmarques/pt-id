a25330cd2c3b1c918111a322631272e566bb7448
António Marques <me@antoniomarques.eu>
2019-04-13 17:28:38 +0100

    Export InvalidArgumentError at top-level

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
   nif: require('./lib/nif.js'),
   bi: require('./lib/bi.js'),
   cc: require('./lib/cc.js'),
-  errors: require('./lib/errors.js')
+  InvalidArgumentError: require('./lib/errors.js').InvalidArgumentError
 }
